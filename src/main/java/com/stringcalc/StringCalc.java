3c9d38561109277103333473dbcb75cd762f00dd
Jignesh Patel <pateljignesh215@gmail.com>
2021-08-28 17:14:48 +0530

    Next commit-Multiple values & Negetive Numbers Handle
    In this commit, written test for handle multiple values with multiple delimiters, and another Test for generate Exception when find Negative Numbers.

--- src/main/java/com/stringcalc/StringCalc.java
+++ src/main/java/com/stringcalc/StringCalc.java
@@ -2,8 +2,8 @@ package com.stringcalc;
 
 public class StringCalc {
 
-	private final String  deli = ",|\n";
-	public int add(String inp) {
+	private final String  deli = ",|\n|;";
+	public int add(String inp) throws Exception {
 		String[] integers = inp.split(deli);
 		if(isEmpty(inp)) {
 			return 0;
@@ -23,12 +23,22 @@ public class StringCalc {
 	private int stringToInt(String inp) {
 		return Integer.parseInt(inp);
 	}
-	private int sumOfTwoNum(String[] integers) {
+	private int sumOfTwoNum(String[] integers) throws Exception {
+		negetiveInputs(integers);
+		
 		int sum =0;
-		for(String curr: integers) {
+		for (String curr : integers){
 			sum += Integer.parseInt(curr);
 			
 		}
 		return sum;
 	}
+		private void negetiveInputs(String[] integers) throws Exception {
+			
+		for(String curr: integers) {
+			if(stringToInt(curr)<0) {
+				throw new Exception("Negetive Input !" +curr);
+			}
+		}
+		}
 }
--- src/test/java/com/stringcalcTest/TestCalculator.java
+++ src/test/java/com/stringcalcTest/TestCalculator.java
@@ -17,26 +17,35 @@ public class TestCalculator {
 	}
 	
 	@Test
-	public void emptyValueReturn() {
+	public void emptyValueReturn() throws Exception {
 		assertEquals(calculator.add(""),0);
 	}
 	@Test
-	public void singleValueReturn() {
+	public void singleValueReturn() throws Exception {
 		assertEquals(calculator.add("1"),1);
 	}
 	@Test
-	public void doubleValueReturn() {
+	public void doubleValueReturn() throws Exception {
 		assertEquals(calculator.add("4,5"), 9);
 	}
 	@Test
-	public void newLineSumReturn() {
+	public void newLineSumReturn() throws Exception {
 		assertEquals(calculator.add("2\n3"), 5);
 	}
 	@Test
-	public void threeValueDelimitedReturn() {
+	public void threeValueDelimitedReturn() throws Exception {
 		assertEquals(calculator.add("2\n3,3"), 8);
-	}@Test
-	public void moreThenTwoValueReturn() {
-		assertEquals(calculator.add("2\n3,3\n2"), 10);
+	}
+	@Test
+	public void moreThenThreeValueReturn() throws Exception {
+		assertEquals(calculator.add("2\n3,3,2"), 10);
+	}
+	@Test
+	public void multipleDelimeterReturn() throws Exception {
+		assertEquals(calculator.add("2\n3;3,2"), 10);
+	}
+	@Test(expected = Exception.class)
+	public void negInputExceptionReturn() throws Exception {
+		calculator.add("-1");
 	}
 }
